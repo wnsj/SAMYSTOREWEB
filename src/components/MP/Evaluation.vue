ce9707c2bc8e245ebd56bba44a577f464420ada0
muwenliang <a56299186@163.com>
2020-03-03 21:33:59 +0800

    添加测评和修改测评

--- src/components/MP/Evaluation.vue
+++ src/components/MP/Evaluation.vue
@@ -6,7 +6,7 @@
 			<h1 class="titleCss">课程管理</h1>
 		</div>
 		<div class="row" style="margin-top: 40px;">
-			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" v-show="accountType==true">
+			<!-- <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" v-show="accountType==true">
 				<div class="col-md-5 col-lg-5 text-right" style="padding: 0; line-height: 34px;">
 					<p class="end-aline col-md-11 col-lg-11" style="padding-right:5px; padding-left:20px;">门店</p><span class="sign-left">:</span>
 				</div>
@@ -41,13 +41,13 @@
 						<option value="0">停用</option>
 					</select>
 				</div>
-			</div>
+			</div> -->
 		</div>
 		<div class="row" style="margin-top: 15px;padding-bottom:1.5%;">
 			<button type="button" class="btn btn-warning pull-right m_r_10" style="margin-right:1.5%;" data-toggle="modal"
 			 v-on:click="selectRule('1')">添加</button>
 			<button type="button" class="btn btn-primary pull-right m_r_10" style="margin-right:1.5%;" data-toggle="modal"
-			 v-on:click="checkProject(1)">查询</button>
+			 v-on:click="checkEvaluation(1)">查询</button>
 		</div>
 		<div class="">
 			<div class="col-md-12 col-lg-12">
@@ -57,25 +57,25 @@
 						<thead class="datathead">
 							<tr>
 								<th class="text-center">ID</th>
-								<th class="text-center">店铺</th>
-								<th class="text-center">咨询师</th>
-								<th class="text-center">课程名称</th>
-								<th class="text-center">单价</th>
-								<th class="text-center">课时(小时)</th>
-								<th class="text-center">优惠比例(%)</th>
+								<th class="text-center">标题</th>
+								<th class="text-center">测试人数(人)</th>
+								<th class="text-center">价格(元)</th>
+								<th class="text-center">题目数(个)</th>
+								<th class="text-center">操作人</th>
 								<th class="text-center">修改</th>
+								<!-- <th class="text-center">添加测评题</th> -->
 							</tr>
 						</thead>
 						<tbody>
-							<tr v-for="(item,index) in projectList" :key="index" v-on:dblclick="selectRule('3',item)">
-								<td class="text-center" style="line-height:33px;">{{item.proId}}</td>
-								<td class="text-center" style="line-height:33px;">{{item.storeName}}</td>
-								<td class="text-center" style="line-height:33px;">{{item.empName}}</td>
-								<td class="text-center" style="line-height:33px;">{{item.proName}}</td>
+							<tr v-for="(item,index) in evaluationList" :key="index" v-on:dblclick="selectRule('3',item)">
+								<td class="text-center" style="line-height:33px;">{{item.tpbId}}</td>
+								<td class="text-center" style="line-height:33px;">{{item.title}}</td>
+								<td class="text-center" style="line-height:33px;">{{item.testNum}}</td>
 								<td class="text-center" style="line-height:33px;">{{item.price}}</td>
-								<td class="text-center" style="line-height:33px;">{{item.frequency}}</td>
-								<td class="text-center" style="line-height:33px;">{{item.discount}}</td>
+								<td class="text-center" style="line-height:33px;">{{item.problemNum}}</td>
+								<td class="text-center" style="line-height:33px;">{{item.price}}</td>
 								<td class="text-center" style="line-height:33px;"><button type="button" class="btn btn-warning" v-on:click="selectRule('3',item)">修改</button></td>
+								<!-- <td class="text-center" style="line-height:33px;"><button type="button" class="btn btn-warning">添加测试题</button></td> -->
 							</tr>
 						</tbody>
 					</table>
@@ -119,12 +119,7 @@
 		},
 		data() {
 			return {
-				projectList: [],
-				isuse: '1',
-				proName: '',
-				storeId: this.storeId(),
-				accountType: this.accountType(),
-				empId: '',
+				evaluationList: [],
 				//分页需要的数据
 				pages: '', //总页数
 				current: 1, //当前页码
@@ -136,47 +131,29 @@
 			//子级传值到父级上来的动态拿去
 			pageChange: function(page) {
 				this.current = page
-				this.checkProject(page);
+				this.checkEvaluation(page);
 			},
-			initData() {
-				this.$refs.emp.setPosName("咨询师")
-				this.$refs.emp.setEmp("")
-			},
+			
 			//feedback from adding and modifying view
 			feedBack() {
-				this.checkProject(1)
+				this.checkEvaluation(1)
 				$("#evaluationContent").modal('hide')
 			},
 
-			storeChange(param) {
-				if (this.isBlank(param)) {
-					this.storeId = ""
-				} else {
-					this.storeId = param.storeId
-				}
-			},
-			empChange(param) {
-				if (this.isBlank(param)) {
-					this.empId = ""
-				} else {
-					this.empId = param.empId
-				}
-			},
 			// check the adding and modifying rule of account
 			selectRule(param, item) {
 				if (param == "1") {
-					this.$refs.subProject.initData('add')
+					this.$refs.subEvaluation.initData('add')
 					$("#evaluationContent").modal('show')
 				} else if (param == "3") {
-					this.$refs.subProject.initData('modify', item)
+					this.$refs.subEvaluation.initData('modify', item)
 					$("#evaluationContent").modal('show')
 				}
 
 			},
 			//check the list of position
-			checkProject(page) {
-				console.log('checkPosition')
-				var url = this.url + '/projects/queryAllByParams'
+			checkEvaluation(page) {
+				var url = this.url + '/testProblemBase/queryTestProblemBaseList'
 				this.$ajax({
 					method: 'POST',
 					url: url,
@@ -185,24 +162,17 @@
 						'Access-Token': this.accessToken
 					},
 					data: {
-						proName: this.proName,
-						state: this.isuse,
-						empId: this.empId,
-						storeId: this.storeId,
+						tpbId: "",
 
-						page: page.toString(),
-						pageSize: this.pageSize
+// 						page: page.toString(),
+// 						pageSize: this.pageSize
 					},
 					dataType: 'json',
 				}).then((response) => {
 					var res = response.data
 					if (res.retCode == '0000') {
-						this.pages = res.retData.pages //总页数
-						this.current = res.retData.current //当前页码
-						this.pageSize = res.retData.size //一页显示的数量
-						this.total = res.retData.total //数据的数量
-						this.$refs.paging.setParam(this.pages, this.current, this.total)
-						this.projectList = res.retData.records
+						this.evaluationList=res.retData
+						// console.log("返回的测评题信息"+JSON.stringify(res))
 					} else {
 						alert(res.retMsg)
 					}
@@ -235,12 +205,11 @@
 			}
 		},
 		mounted() {
-			this.initData()
 			window.addEventListener('scroll', this.handleScroll, true);
 			init();
 		},
 		created() {
-			// this.checkProject(1)
+			this.checkEvaluation(1)
 		}
 	}
 </script>
--- src/components/MP/SubProject/SubEvaluation.vue
+++ src/components/MP/SubProject/SubEvaluation.vue
@@ -3,62 +3,38 @@
 	<div class="modal-content">
 		<div class="modal-header">
 			<button type="button" aria-hidden="true" class="close" v-on:click="closeCurrentPage()">×</button>
-			<h4 id="myModalLabel" class="modal-title">{{title}}项目</h4>
+			<h4 id="myModalLabel" class="modal-title">{{title}}测评</h4>
 		</div>
 		<div class="modal-body  pos_r">
 			<div class="tab-pane fade in active martop" id="basic">
 				<form action="" class="clearfix">
 					<div class="col-md-6 form-group clearfix">
-						<label for="cyname" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">课程名称</label><span class="sign-left">:</span>
+						<label for="cyname" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">测评标题</label><span class="sign-left">:</span>
 						<div class="col-md-8">
-							<input type="text" class="form-control" v-model="project.proName" placeholder="">
+							<input type="text" class="form-control" v-model="evaluation.title" placeholder="">
 						</div>
 					</div>
-					<div class="col-md-6 form-group clearfix">
+					<!-- <div class="col-md-6 form-group clearfix">
 						<label for="cyname" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">咨询师</label><span class="sign-left">:</span>
 						<div class="col-md-8">
 							<emp ref='emp' @employeeChange='empChange'></emp>
 						</div>
-					</div>
+					</div> -->
 					<div class="col-md-6 form-group clearfix">
 						<label for="cyname" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">单价</label><span class="sign-left">:</span>
 						<div class="col-md-8">
-							<input type="text" class="form-control" v-model="project.price" placeholder="">
+							<input type="text" class="form-control" v-model="evaluation.price" placeholder="">
 							<span class="pos-ab pos-tr">¥</span>
 						</div>
 					</div>
 					<div class="col-md-6 form-group clearfix">
-						<label for="cyname" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">课时</label><span class="sign-left">:</span>
+						<label for="cyname" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">题数</label><span class="sign-left">:</span>
 						<div class="col-md-8">
-							<input type="text" class="form-control" v-model="project.frequency">
-							<span class="pos-ab pos-tr">H</span>
+							<input type="text" class="form-control" v-model="evaluation.problemNum">
+							<span class="pos-ab pos-tr">个</span>
 						</div>
 					</div>
-					<div class="col-md-6 form-group clearfix">
-						<label for="cyname" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">优惠比例</label><span class="sign-left">:</span>
-						<div class="col-md-8 pos-re">
-							<input type="text" class="form-control" v-model="project.discount" placeholder="">
-							<span class="pos-ab pos-tr">%</span>
-						</div>
-					</div>
-					<div class="col-md-6 form-group clearfix">
-						<label for="erpzh" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">是否停用</label><span class="sign-left">:</span>
-						<div class="col-md-8">
-							<select class="form-control" v-model="project.state">
-								<option value="1">在用</option>
-								<option value="0">停用</option>
-							</select>
-						</div>
-					</div>
-					<div class="col-md-6 form-group clearfix">
-						<label for="erpzh" class="col-md-3 control-label text-right nopad end-aline" style="padding:0;line-height:34px;">体验课</label><span class="sign-left">:</span>
-						<div class="col-md-8">
-							<select class="form-control" v-model="project.proType">
-								<option value="1">否</option>
-								<option value="0">是</option>
-							</select>
-						</div>
-					</div>
+					
 					<div class="form-group clearfix">
 						<div class="col-md-12">
 							<button type="button" class="btn btn-warning pull-right m_r_10" style="margin-right:1.5%;" data-toggle="modal"
@@ -85,82 +61,63 @@
 		},
 		data() {
 			return {
-				project: {
-					proName: '',
-					empId: '',
-					state: '1',
-					price:'',
-					frequency:'',
-					discount:'',
-					proType:'0',
+				evaluation: {
+					tpbId:'',
+					title: '',
+					price: '',
+					problemNum:'',
 				},
 				title: '',
 			};
 		},
 		methods: {
 			// Initialization projcet’s content
-			initData(param, project) {
+			initData(param, evaluation) {
 				if (param == 'add') {
-					console.log('Initialization project’s content, which adds project')
+					console.log('Initialization evaluation’s content, which adds evaluation')
 					this.title = '新增'
-					this.project = {
-						proName: '',
-						empId: '',
-						state: '1',
-						price:'',
-						proType:'1',
-						frequency:'',
-						discount:'',
-						storeId:this.storeId()
+					this.evaluation = {
+						tpbId:'',
+						title: '',
+						price: '',
+						problemNum:'',
 					}
-					this.$refs.emp.setPosName("咨询师")
-					this.$refs.emp.setEmp("")
 				} else if (param == 'modify') {
-					console.log('Initialization project’s content, which modifies project')
+					console.log('Initialization evaluation’s content, which modifies evaluation')
 					this.title = '修改'
-					Object.assign(this.project,project)
-					this.$refs.emp.setPosName("咨询师")
-					this.$refs.emp.setEmp(this.project.empId)
+					Object.assign(this.evaluation,evaluation)
 				}
 			},
-			
-			empChange(param){
-				if(this.isBlank(param)){
-					this.project.empId=""
-				}else{
-					this.project.empId=param.empId
-				}
-			},
+// 			
+// 			empChange(param){
+// 				if(this.isBlank(param)){
+// 					this.evaluation.empId=""
+// 				}else{
+// 					this.evaluation.empId=param.empId
+// 				}
+// 			},
 			//the event of addtional button
 			certainAction() {
 				console.log('the event of addtional button')
 				
-				if (this.isBlank(this.project.proName)) {
-					alert("课程名称不能为空")
+				if (this.isBlank(this.evaluation.title)) {
+					alert("标题不能为空")
 					return
 				}
-				if (this.isBlank(this.project.empId)) {
-					alert("咨询师不能为空")
+				if (this.isBlank(this.evaluation.price)) {
+					alert("价格不能为空")
 					return
 				}
-				if (this.isBlank(this.project.price) || this.project.price < 0) {
-					alert("课时单价不能为空")
-					return
-				}
-				if (this.isBlank(this.project.frequency)) {
-					alert("课时不能为空")
-					return
-				}
-				if (this.isBlank(this.project.discount)) {
-					alert("折扣不能为空")
+				if (this.isBlank(this.evaluation.problemNum)) {
+					alert("问题个数不能为空")
 					return
 				}
 				switch(this.title){
 					case '新增':
-						var url = this.url+'/projects/addProjectName'
+						var url = this.url+'/testProblemBase/addTestProblemBase'
 						break;
 					case '修改':
-						var url = this.url+'/projects/updateProjectName'
+						var url = this.url+'/testProblemBase/updateTestProblemBase'
 						break;
 				}
 				
@@ -171,11 +128,10 @@
 						'Content-Type': this.contentType,
 						'Access-Token': this.accessToken
 					},
-					data: this.project,
+					data: this.evaluation,
 					dataType: 'json',
 				}).then((response) => {
 					var res = response.data
-					console.log(res)
 					if (res.retCode == '0000') {
 						alert(res.retMsg)
 						this.$emit('certainAction')
@@ -185,7 +141,7 @@
 				});
 			},
 			closeCurrentPage() {
-				$("#projectContent").modal("hide")
+				$("#evaluationContent").modal("hide")
 				console.log('关闭添加课程界面')
 			},
 		}
