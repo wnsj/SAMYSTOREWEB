321c73c84381f00745e392e97efa58936c6dab74
1332165192 <1332165192@qq.com>
2020-03-25 14:32:28 +0800

    提交

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,8 +5,8 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-	//API_HOST:'"http://172.16.56.1:8080"',
-	API_HOST:'"http://172.16.3.21:8080"',
+	API_HOST:'"http://172.16.56.1:8080"',
+	//API_HOST:'"http://172.16.3.21:8080"',
 	// API_HOST:'"http://172.16.213.210:8080/samy"',
 	// API_HOST:'"https://172.16.213.210/samy"',
 	//API_HOST:'"http://172.16.3.116:8080/"'
