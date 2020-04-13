77df15d11e98507e338c35d8d94d911757823dd7
liaomupeng <521040317@qq.com>
2020-04-13 15:13:41 +0800

    20200413保存

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,8 +7,9 @@ module.exports = merge(prodEnv, {
 
 	// API_HOST:'"http://172.16.56.1:8080"',
 	// API_HOST:'"http://172.16.3.21:8080"',
-	API_HOST:'"http://111.160.38.141:4092/samystore"',
+	//API_HOST:'"http://111.160.38.141:4092/samystore"',
 	// API_HOST:'"http://172.16.213.210:8080/samy"',
 	// API_HOST:'"https://172.16.213.210/samy"',
 	//API_HOST:'"http://172.16.3.116:8080/"'
+	API_HOST:'"http://172.16.3.67:8080"'
 })
