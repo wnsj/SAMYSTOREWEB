816489056bc7016ca98f579a65c9f360e10b8956
sunyl-5664 <lx5664@hotmail.com>
2020-06-15 10:53:40 +0800

    密码初始化

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,8 +6,8 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
 	// API_HOST:'"http://172.16.56.1:8080"',
-	API_HOST:'"http://172.16.3.92:8080"',
-	// API_HOST:'"http://172.16.3.33:8080"',
+	// API_HOST:'"http://172.16.3.92:8080"',
+	API_HOST:'"http://172.16.3.33:8080"',
 	// API_HOST:'"http://vip.miyuexli.com:88/samystore"',
 	// API_HOST:'"http://172.16.213.210:8080/samy"',
 	// API_HOST:'"https://111.160.38.141:4093/samystore"',
